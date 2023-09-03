5786ae6aa873cd385f839928e528ca1e56183f4f
liunian-zy <a@liunian.io>
2023-09-04 02:38:24 +0800

    first commit

---
+++ src/widgets/WalletModal/config.tsx
@@ -0,0 +1,19 @@
+import Metamask from "./icons/Metamask";
+import MathWallet from "./icons/MathWallet";
+import TokenPocket from "./icons/TokenPocket";
+import TrustWallet from "./icons/TrustWallet";
+import WalletConnect from "./icons/WalletConnect";
+import BinanceChain from "./icons/BinanceChain";
+import SafePalWallet from "./icons/SafePalWallet";
+import { Config, ConnectorNames } from "./types";
+
+const connectors: Config[] = [
+  {
+    title: "PhiWallet",
+    icon: Metamask,
+    connectorId: ConnectorNames.Injected,
+  },
+];
+
+export default connectors;
+export const connectorLocalStorageKey = "connectorId";
